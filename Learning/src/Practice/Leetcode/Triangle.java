bb2d938d68df938d39b5aaefdc220f852954747e
ssh <1015096352@qq.com>
2021-08-25 00:43:51 +0800

    动态规划 halfWork

---
+++ Learning/src/Practice/Leetcode/Triangle.java
@@ -0,0 +1,20 @@
+package Practice.Leetcode;
+
+import java.util.List;
+
+/**
+ * @Author ShaoHua Sun
+ * @Description 120.三角形最短路径
+ * @Date 2021/8/24 23:59
+ */
+public class Triangle {
+
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        int sum = 0;
+        int index = 0;
+        for (int i = 0; i < triangle.size(); i++) {
+
+        }
+        return 0;
+    }
+}
